4bf740c30ffd6e67406b4f095b7507d2ae8ee58c
@krishnapada <krishnamandalpada@gmail.com>
2024-12-02 16:27:25 +0530

    addd

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useAuthStore } from '../store/useAuthStore'
 
 const SignUpPage = () => {
+  const [isPassword,showPassword] = useState(false)
   const [formData,SetFormData] = useState({
     full_name:"",
     email:"",
@@ -11,8 +12,11 @@ const SignUpPage = () => {
   const validationFrom = () =>{}
   const handleSubmit = ()=>{}
   return (
-    <div>
-      <h1>Sign-up page component</h1>
+    <div className='min-h-screen grid lg:grid-col-2'>
+      <div className='flex flex-col justify-center item-center p-6 sm:p-12'>
+
+      </div>
+      
     </div>
   )
 }
